15a6c3ceca26ae15abc18ab6df63cf24d90802fa
nadya-hub <54092503+nadya-hub@users.noreply.github.com>
2019-08-22 18:39:51 +0300

    Update Search test

--- New-first/src/test/java/SearchTest.java
+++ New-first/src/test/java/SearchTest.java
@@ -1,11 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SearchTest {
     WebDriver driver;
     @Test
     public void searchWikiTest() throws InterruptedException {
+        driver = new ChromeDriver();
         driver.get("https://en.wikipedia.org");
         driver.findElement(By.name("search")).click();
         driver.findElement(By.name("search")).sendKeys("selenium");
