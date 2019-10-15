06969cdfd1fdad21a6d26a9fbeaae080b8df73ed
nadya-hub <54092503+nadya-hub@users.noreply.github.com>
2019-10-15 15:50:51 +0300

    Added LogBack

--- New-first/src/test/java/com/telran/selenium/tests/LoginToWiki.java
+++ New-first/src/test/java/com/telran/selenium/tests/LoginToWiki.java
@@ -1,9 +1,25 @@
 package com.telran.selenium.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginToWiki extends TestBase {
+    Logger logger = LoggerFactory.getLogger(LoginToWiki.class );
+    @BeforeMethod
+    public void startLogger(){
+        logger.info("Start  test LoginToWiki ");
+
+    }
+
+    @AfterMethod
+    public void stopLogger(){
+        logger.info("Stop test LoginToWiki");
+    }
+
     @BeforeClass
     public  void ensurePredicationOpenSite(){
         app.getSessionHelper().openSiteWiki();
